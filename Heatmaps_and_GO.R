b8595a76384289ccd7d4f9d1b50f1ce37d72d528
yazahmed <ya76@cornell.edu>
2016-12-27 16:32:47 -0500

    Initial commit

---
+++ Heatmaps_and_GO.R
@@ -0,0 +1,129 @@
+
+# create gene lists and factor labeling
+RT_factors = as.data.frame(Dnov.dvir1.06.RT.list)
+RT_factors$V1 = "RT-biased"
+rownames(RT_factors) = Dnov.dvir1.06.RT.list
+RT_factors = subset(RT_factors, select = "V1")
+
+OV_factors = as.data.frame(Dnov.dvir1.06.OV.list)
+OV_factors$V1 = "OV-biased"
+rownames(OV_factors) = Dnov.dvir1.06.OV.list
+OV_factors = subset(OV_factors, select = "V1")
+
+H_factors = as.data.frame(Dnov.dvir1.06.H.list)
+H_factors$V1 = "H-biased"
+rownames(H_factors) = Dnov.dvir1.06.H.list
+H_factors = subset(H_factors, select = "V1")
+
+factor_labeling = rbind(RT_factors, OV_factors, H_factors)
+colnames(factor_labeling) = c('type')
+factor_list = unique(factor_labeling[,1])
+
+
+## For heatmap, do this:
+Sig.Tissue.Biased = subset(Dnov_virgin_tissue_MeanTPMmatrix, rownames(Dnov_virgin_tissue_MeanTPMmatrix) %in% rownames(factor_labeling))
+
+data = Sig.Tissue.Biased
+gene_factors = unique(factor_labeling[,1])
+gene_factor_colors = rainbow(length(gene_factors))
+names(gene_factor_colors) = gene_factors
+data = log2(data+1)
+data = as.matrix(data) # convert to matrix
+sample_cor = cor(data, method='pearson', use='pairwise.complete.obs')
+sample_dist = as.dist(1-sample_cor)
+hc_samples = hclust(sample_dist, method='complete')
+gene_cor = NULL
+if (is.null(gene_cor)) { gene_cor = cor(t(data), method='pearson', use='pairwise.complete.obs') }
+gene_dist = as.dist(1-gene_cor)
+hc_genes = hclust(gene_dist, method='complete')
+myheatcol = colorpanel(75, '#f8e95a','black','#e1526d')
+data = t(scale(t(data), scale=F)) # center rows, mean substracted
+heatmap_data = data
+gene_factor_row_vals = as.factor(factor_labeling[rownames(heatmap_data),])
+names(gene_factor_row_vals) = rownames(heatmap_data)
+gene_factors_here = unique(gene_factor_row_vals)
+names(gene_factors_here) = gene_factors_here
+num_gene_factors_here = length(gene_factors_here)
+geneFactorColors = c("#e1526d", "#b85516", "#647700")
+geneFactorAnnotations = matrix(nrow=nrow(heatmap_data), ncol=num_gene_factors_here)
+for (i in 1:num_gene_factors_here) {
+    geneFactorAnnotations[,i] = gene_factor_row_vals %in% gene_factors_here[i]
+}
+geneFactorAnnotations = apply(geneFactorAnnotations, 1:2, function(x) as.logical(x))
+geneFactorAnnotations = t(sample_matrix_to_color_assignments(t(geneFactorAnnotations), col=geneFactorColors))
+rownames(geneFactorAnnotations) = rownames(heatmap_data)
+colnames(geneFactorAnnotations) = gene_factors_here
+heatmap_data[heatmap_data < -4] = -4
+heatmap_data[heatmap_data > 4] = 4
+
+## plot it
+heatmap.3(heatmap_data, dendrogram='both', Rowv=as.dendrogram(hc_genes), Colv=as.dendrogram(hc_samples), col=myheatcol, scale="none", density.info="none", trace="none", key=TRUE, keysize=1.5, cexCol=3, margins=c(10,10), cex.main=0.75, RowSideColors = geneFactorAnnotations)
+
+
+
+### For GO analysis:
+
+# build pwf based on ALL DE features
+cat_genes_vec = as.integer(features_with_GO %in% rownames(factor_labeling))
+pwf=nullp(cat_genes_vec,bias.data=lengths_features_with_GO)
+rownames(pwf) = names(GO_info_listed)
+
+GO_enriched_list = list()
+
+for (feature_cat in factor_list) {
+    message('Processing category: ', feature_cat)
+    cat_genes_vec = as.integer(features_with_GO %in% rownames(factor_labeling)[factor_labeling$type == feature_cat])
+    pwf$DEgenes = cat_genes_vec
+    res = goseq(pwf,gene2cat=GO_info_listed)
+    ## over-represented categories:
+    pvals = res$over_represented_pvalue
+    pvals[pvals > 1 -1e-10] = 1-1e-10
+    q = qvalue(pvals)
+    res$over_represented_FDR = q$qvalues
+    go_enrich_factor = feature_cat
+    enrich_result_table = res[res$over_represented_pvalue<=0.05,]
+    descr = unlist(lapply(enrich_result_table$category, get_GO_term_descr))
+    enrich_result_table$go_term = descr
+    enrich_result_table$factor = go_enrich_factor
+    GO_enriched_list[[feature_cat]] = enrich_result_table
+}
+
+GO_enrichment_data = rbindlist(GO_enriched_list)
+
+
+
+### RT plot
+ggplot(subset(GO_enrichment_data, over_represented_FDR < 0.05 & factor == "RT-biased"), 
+       aes(category, -log10(over_represented_pvalue), size = numDEInCat, colour = ontology)) + 
+    geom_point()  + 
+    xlab(NULL) + 
+    geom_text_repel(data = subset(GO_enrichment_data, numDEInCat > 20 & factor == "RT-biased" & ontology == 'MF'), 
+                    aes(category, -log10(over_represented_pvalue),label=term), 
+                    force = 8, 
+                    inherit.aes = F, 
+                    box.padding = unit(0.35, "lines"), 
+                    point.padding = unit(0.5, "lines"), 
+                    fontface = "bold", 
+                    size = 3) + 
+    theme(axis.text.x = element_text(angle = 45, face = "bold", vjust = 1, hjust = 1)) + 
+    scale_size(range = c(0,12)) + 
+    scale_colour_manual(values=c("#c27d92", "#a8a34b", "#8e61bf")) + 
+    scale_y_continuous(limits=c(3, 10))
+
+
+ggplot(subset(GO_enrichment_data, over_represented_FDR < 0.001 & factor == "H-biased"), 
+       aes(category, -log10(over_represented_pvalue), size = numDEInCat, colour = ontology)) + 
+    geom_point()  + 
+    xlab(NULL) + 
+    geom_text_repel(data = subset(GO_enrichment_data, over_represented_FDR < 0.000001 & factor == "H-biased"), 
+                    aes(category, -log10(over_represented_pvalue),label=term), 
+                    force = 8, 
+                    inherit.aes = F, 
+                    box.padding = unit(0.35, "lines"), 
+                    point.padding = unit(0.5, "lines"), 
+                    fontface = "bold", 
+                    size = 3) + 
+    theme(axis.text.x = element_text(angle = 45, face = "bold", vjust = 1, hjust = 1)) + 
+    scale_size(range = c(0,12)) + 
+    scale_colour_manual(values=c("#c27d92", "#a8a34b", "#8e61bf")) + 
+    scale_y_continuous(limits=c(3, 10))
